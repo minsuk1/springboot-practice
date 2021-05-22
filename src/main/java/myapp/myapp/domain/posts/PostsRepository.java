a6ae1e11898555401082a951189013abe41b25c1
minsuk <palt1230@naver.com>
2021-05-22 22:07:50 +0900

    post 테이블 추가

--- src/main/java/myapp/myapp/domain/posts/PostsRepository.java
+++ src/main/java/myapp/myapp/domain/posts/PostsRepository.java
@@ -2,6 +2,6 @@ package myapp.myapp.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-//JpaRepository<Entitu Class, PK타입>
+//JpaRepository<Entity Class, PK타입>
 public interface PostsRepository extends JpaRepository<Posts, Long> {
 }
