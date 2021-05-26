ccd04603151a5250ae8fc5cfa8362c3574bdd2f0
minsuk <palt1230@naver.com>
2021-05-26 19:08:03 +0900

    google 로그인 추가

--- src/main/java/myapp/myapp/web/IndexController.java
+++ src/main/java/myapp/myapp/web/IndexController.java
@@ -1,6 +1,7 @@
 package myapp.myapp.web;
 
 import lombok.RequiredArgsConstructor;
+import myapp.myapp.config.auth.dto.SessionUser;
 import myapp.myapp.service.PostsService;
 import myapp.myapp.web.dto.PostsResponseDto;
 import org.springframework.stereotype.Controller;
@@ -8,15 +9,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+       if (user != null){
+           model.addAttribute("userName", user.getName());
+       }
         return "index";
     }
 
