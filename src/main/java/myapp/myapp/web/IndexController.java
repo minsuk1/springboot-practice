82525a7443455af079aa16de279eed916bee48da
minsuk <palt1230@naver.com>
2021-05-27 15:22:20 +0900

    어노테이션 등록

--- src/main/java/myapp/myapp/web/IndexController.java
+++ src/main/java/myapp/myapp/web/IndexController.java
@@ -1,6 +1,7 @@
 package myapp.myapp.web;
 
 import lombok.RequiredArgsConstructor;
+import myapp.myapp.config.auth.LoginUser;
 import myapp.myapp.config.auth.dto.SessionUser;
 import myapp.myapp.service.PostsService;
 import myapp.myapp.web.dto.PostsResponseDto;
@@ -19,9 +20,9 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        // SessionUser user = (SessionUser) httpSession.getAttribute("user");
        if (user != null){
            model.addAttribute("userName", user.getName());
        }
